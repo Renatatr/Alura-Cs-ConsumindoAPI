db480923e27dac2742ad92d38b61855ee209c093
Renatatr <78363362+Renatatr@users.noreply.github.com>
2023-08-21 15:39:18 -0300

    Initial Commit

---
+++ SoundScree4/Program.cs
@@ -0,0 +1,12 @@
+﻿using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"Temos um problema: {e.Message}");
+    }
+    
+}
